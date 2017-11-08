282e97cbff100dd0238128f805283d2272d5b19d
Zelenko <30665185+zelenko@users.noreply.github.com>
2017-11-08 08:48:06 -0500

    Go connections!

---
+++ 11_socket/tcp-client.go
@@ -0,0 +1,23 @@
+package main
+
+import "net"
+import "fmt"
+import "bufio"
+import "os"
+
+func main() {
+
+	// connect to this socket
+	conn, _ := net.Dial("tcp", "127.0.0.1:8081")
+	for {
+		// read in input from stdin
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Text to send: ")
+		text, _ := reader.ReadString('\n')
+		// send to socket
+		fmt.Fprintf(conn, text+"\n")
+		// listen for reply
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		fmt.Print("Message from server: " + message)
+	}
+}
---
+++ 11_socket/tcp-server.go
@@ -0,0 +1,30 @@
+package main
+
+import "net"
+import "fmt"
+import "bufio"
+import "strings" // only needed below for sample processing
+
+func main() {
+
+	fmt.Println("Launching server...")
+
+	// listen on all interfaces
+	ln, _ := net.Listen("tcp", ":8081")
+
+	// accept connection on port
+	conn, _ := ln.Accept()
+
+	// run loop forever (or until ctrl-c)
+	for {
+		// will listen for message to process ending in newline (\n)
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		// output message received
+		fmt.Print("Message Received:", string(message))
+		// sample process for string received
+		newmessage := strings.ToUpper(message)
+		// send new string back to client
+		conn.Write([]byte(newmessage + "\n"))
+
+	}
+}
