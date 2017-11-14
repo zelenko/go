7a31dc05af1393d8fe7c3ddfd40d3060bbe90ae6
zelenko <30665185+zelenko@users.noreply.github.com>
2017-11-13 19:22:32 -0500

    QR barcode generator

---
+++ 13_qr/main.go
@@ -0,0 +1,72 @@
+// http://localhost:8080/?data=https://github.com/zelenko/go&size=150
+
+package main
+
+import (
+	"bytes"
+	"github.com/boombuler/barcode"
+	"github.com/boombuler/barcode/qr"
+	"image/png"
+	"log"
+	"net/http"
+	"net/url"
+	"strconv"
+)
+
+func main() {
+	http.HandleFunc("/", QrGenerator)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func QrGenerator(w http.ResponseWriter, r *http.Request) {
+	data := r.URL.Query().Get("data")
+	if data == "" {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	s, err := url.QueryUnescape(data)
+	if err != nil {
+		http.Error(w, "", http.StatusBadRequest)
+		return
+	}
+
+	code, err := qr.Encode(s, qr.L, qr.Auto)
+	if err != nil {
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+
+	size := r.URL.Query().Get("size")
+	if size == "" {
+		size = "250"
+	}
+	intsize, err := strconv.Atoi(size)
+	if err != nil {
+		intsize = 250
+	}
+
+	// Scale the barcode to the appropriate size
+	code, err = barcode.Scale(code, intsize, intsize)
+	if err != nil {
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+
+	buffer := new(bytes.Buffer)
+	if err := png.Encode(buffer, code); err != nil {
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "image/png")
+	w.Header().Set("Content-Length", strconv.Itoa(len(buffer.Bytes())))
+
+	if _, err := w.Write(buffer.Bytes()); err != nil {
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+}
