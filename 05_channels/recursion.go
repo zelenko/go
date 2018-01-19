d9bc54b77ba02d56b83e156435491be8f467e176
Zelenko <30665185+zelenko@users.noreply.github.com>
2018-01-18 23:22:08 -0500

    In Go, the code does exactly what it says on the page.

---
+++ 05_channels/recursion.go
@@ -0,0 +1,34 @@
+// Examples of Recursion in Go
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+// factorial recursion
+func factorial(i int) int {
+	if i <= 1 {
+		return 1
+	}
+	return i * factorial(i-1)
+}
+
+// countdown return decremental list of numbers
+func countdown(i int, s []string) (int, []string) {
+	if i < 1 {
+		return i, s
+	}
+
+	return countdown(i-1, append(s, strconv.Itoa(i)))
+}
+
+// main is the entry point for the program.
+func main() {
+	var i int = 15
+	fmt.Printf("Factorial of %d is %d\n", i, factorial(i))
+
+	_, s := countdown(i, []string{})
+	fmt.Println("Countdown:", s)
+}
