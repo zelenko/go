3f73ec39e53035266b1facc5003b88bbb1078ce9
zelenko <30665185+zelenko@users.noreply.github.com>
2018-02-07 17:03:36 -0500

    content directory

---
+++ 39_read_directory_content/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
+func main() {
+	searchFiles("../")
+}
+
+func searchFiles(dir string) { // dir is the parent directory you what to search
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		if(!file.IsDir()){
+			fmt.Println(" --",file.Name())
+
+		} else{
+			fmt.Println(file.Name())
+		}
+		
+	}
+}
+
+/*
+https://golang.org/pkg/os/#FileInfo
+
+type FileInfo interface {
+        Name() string       // base name of the file
+        Size() int64        // length in bytes for regular files; system-dependent for others
+        Mode() FileMode     // file mode bits
+        ModTime() time.Time // modification time
+        IsDir() bool        // abbreviation for Mode().IsDir()
+        Sys() interface{}   // underlying data source (can return nil)
+}
+*/
---
+++ 40_cron_scheduler/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/mileusna/crontab"
+)
+
+func main() {
+
+	ctab := crontab.New() // create cron table
+
+	// MustAddJob is like AddJob but panics on wrong syntax or problems with func/args
+	// use for easier initialization
+	ctab.MustAddJob("* * * * *", myFunc)     // every minute
+	ctab.MustAddJob("0 12 * * *", myFunc3)   // noon lauch
+	ctab.MustAddJob("/2 * * * *", myFunc2, "Monday and Tuesday midnight", 123) // run every two seconds
+
+	// fn with args
+	ctab.MustAddJob("0 0 * * 1,2", myFunc2, "Monday and Tuesday midnight", 123)
+	ctab.MustAddJob("*/5 * * * *", myFunc2, "every five min", 0)
+
+	// or use AddJob if you want to test the error
+	err := ctab.AddJob("0 12 1 * *", myFunc) // on 1st day of month
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	// all your other app code as usual, or put sleep timer for demo
+	 time.Sleep(10 * time.Minute)
+	 // time.Sleep(5 * time.Second)
+}
+
+func myFunc() {
+	fmt.Println("Helo, world")
+}
+
+func myFunc3() {
+	fmt.Println("Noon!")
+}
+
+func myFunc2(s string, n int) {
+	//fmt.Println("We have params here, string", s, "and number", n)
+	fmt.Println(time.Now(), "- just ticked")
+
+}
