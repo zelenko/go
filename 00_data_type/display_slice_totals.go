1f6f811b1172307899a5bd675144b060e90e4ed0
zelenko <30665185+zelenko@users.noreply.github.com>
2018-02-13 13:47:35 -0500

    In Go, the code does exactly what it says on the page.

---
+++ 00_data_type/display_slice_totals.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	quiz([]int{11, 9, 6, 11, 5}, 3)       // expected answer "26, 16"
+	quiz([]int{11, 9, 6, 11, 5, 1}, 3)    // expected answer "26, 17"
+	quiz([]int{11, 9, 6, 11, 5, 1, 1}, 3) // expected answer "26, 17, 1"
+}
+
+// quiz sums every n elements of arr and print.
+// also print the sum of any remaining elements
+func quiz(arr []int, n int) {
+
+	out := 0
+
+	for i := 0; i < len(arr); i++ {
+		out += arr[i]
+
+		if (i+1)%n == 0 {
+			fmt.Printf("%d, ", out)
+			out = 0
+		} else if i+1 == len(arr) {
+			fmt.Printf("%d ", out)
+			out = 0
+		}
+
+	}
+	fmt.Println("")
+}
