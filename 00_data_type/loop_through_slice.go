63778c30842d241bc0f77a5d0783b869e605c21b
zelenko <30665185+zelenko@users.noreply.github.com>
2018-02-28 18:08:16 -0500

    types, constructors, variables, and function

---
+++ 00_data_type/loop_through_slice.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+type (
+	rec string
+)
+
+const (
+	test = "collection"
+)
+
+var (
+	list   = []rec{"a", "b"}
+	output = []rec{"a", "b"}
+)
+
+func main() {
+
+	output = append(output, "c")
+
+	// multiple ways to loop through slice
+	for i := range output {
+		fmt.Println("i:", output[i], i)
+	}
+
+	for k, j := range output {
+		fmt.Println("k:", k, j)
+	}
+
+	m := 0
+	for range output {
+		fmt.Println("m:", output[m])
+		m++
+	}
+
+	for n := 0; n < len(output); n++ {
+		fmt.Println("n:", output[n])
+	}
+
+	fmt.Println(test)
+}
