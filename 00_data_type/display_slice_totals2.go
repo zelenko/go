a35fa27616936d0267a0c269239cbc6b5447a775
zelenko <30665185+zelenko@users.noreply.github.com>
2018-03-06 19:11:30 -0500

    Multiple ways to achive the same goal

---
+++ 00_data_type/display_slice_totals2.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	numbers := []int{1,2,3,4,6,7}
+
+	check(numbers, 2)
+}
+
+func check(number []int, total int){
+	j := 0
+	for i := range number{
+		j = number[i] + j
+		//fmt.Print(number[i], total, "\n")
+		if  (i+1)%total == 0{
+			fmt.Print(j,",")
+			j = 0
+		}
+	}
+}
